d95ba560d79831507eedaee1cc8503aac57d43e8
Animila <hristoforovila5@gmail.com>
2022-05-11 22:27:11 +0900

    Rename package

--- lesson14/configIMG.py
+++ lesson14/configIMG.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file '.\GUI_lessons\college\lesson_14\img.ui'
+# Form implementation generated from reading ui file '.\GUI_lessons\college\lesson14\img.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.4
 #
